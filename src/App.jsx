25fb3e87a0fcb30164ef2832c9d2bb3652b366d3
nurillo00 <nurillobest33@gmail.com>
2024-03-29 11:02:53 +0500

    second commit of React-project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import './App.css'
+import HeroImg from "./assets/img/hero-bg.png"
+import InfoImg from "./assets/img/info-img.png"
 
 function App() {
 
@@ -31,12 +33,24 @@ function App() {
 
           <section className='hero'>
             <div className="hero-wrap">
-              <div className="hero-text">
+              <div className="hero-text-box">
                 <h1 className='hero-title'>Nature, Warmth, and Beauty in One Space</h1>
-                <p>One place to release all the stress, bring back happines, and get back to nature. We provide the best room and nature for you. Come visit us anytime you want.</p>
-                <a href="">Reservation</a>
+                <p className='hero-text'>One place to release all the stress, bring back happines, and get back to nature. We provide the best room and nature for you. Come visit us anytime you want.</p>
+                <a className='hero-link' href="">Reservation</a>
               </div>
-              <img src="" alt="" />
+              <img className='hero-img' src={HeroImg} alt="hero-img" />
+            </div>
+          </section>
+
+          <section className='info'>
+            <div className="info-wrap">
+              <img src={InfoImg} alt="" />
+              <div className="info-text-box">
+                <h2 className='info-title'>Cozy and Down to Earth Cootage Hotel in Norway.</h2>
+                <p className='info-text'>Our Cootage Hotel is an intimate hideway concieved for dicerning travelers. It faces directly the unique and spectacular panorama of the Hallerbos Jungle.</p>
+                <p className='info-text2'>Cootels extends along a private, quiet, and beautiful nature. Stay away from crowd and enjoy the cozy and beauty with us.</p>
+                <a className='info-link' href="">Learn More</a>
+              </div>
             </div>
           </section>
 
